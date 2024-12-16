4ec95e2fe7cbc46ec1b003f926806fc5a0f63aee
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2024-12-16 23:57:31 +0545

    type promotion

---
+++ src/com/typecasting/TypePromotion.java
@@ -0,0 +1,10 @@
+package com.typecasting;
+
+public class TypePromotion {
+    public static void main(String[] args) {
+        byte b = 123;
+        byte c = 23;
+        int result = b+c;
+        System.out.println(result);
+    }
+}
