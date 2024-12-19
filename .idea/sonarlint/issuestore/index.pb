e98335d7aa0fa6a6988f8d52274876562a5b5c87
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2024-12-19 23:03:50 +0545

    basic array

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+Q
+!src/com/classandmethods/Main.java,1\a\1afd27aea40b00979b85aef60cb3399d00af4618
+]
+-src/com/conditionalstatement/LoopsInJava.java,4\a\4a2e78305fcc52b0bdf81d0db31cf702c0cdaf98
+a
+1src/com/conditionalstatement/SwitchStatement.java,7\4\7411e86f5621576ba4f847e075a0d7856e219aad
+n
+>src/com/conditionalstatement/ConditionalStatementsExample.java,d\1\d1e1c02211cc176bfd4ebf366513d90ca503b275
---
+++ src/com/arrays/Demo.java
@@ -0,0 +1,21 @@
+package com.arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int nums[] = new int[4];
+
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+
+        for (int i =0;i<=3;i++){
+            System.out.println(nums[i]);
+        }
+        System.out.println("================");
+        for(int n : nums){
+            System.out.println(n);
+        }
+    }
+}
