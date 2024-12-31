8138128eb8ff68131ec4a1024e00675c21aea6c6
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2024-12-31 20:46:34 +0545

    reverse test

--- src/com/questions/ReverseStringJava.java
+++ src/com/questions/ReverseStringJava.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class ReverseStringJava {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter string to reverse");
+//        String str = sc.nextLine();
+//        System.out.print("Reverse String is : ");
+//        System.out.println(reverseString(str));
+//    }
+//    public static String reverseString(String str){
+//        return new StringBuilder(str).reverse().toString();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter string to reverse");
         String str = sc.nextLine();
